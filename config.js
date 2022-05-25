bb0ca2cec52ecab510f1a0558bfd235c7ae6979b
littleforest <670177821@qq.com>
2022-05-25 15:40:32 +0800

    feat: 修改config入口

--- config.js
+++ config.js
@@ -1,17 +1,13 @@
 const waffle = require(`${process.cwd()}/.waffle.ts`);
 const autoEntryCore = require('./auto-entry/core')
-const rollup = require('rollup');
-const rollupUmd = require('./compile-way/rollup/rollup.config.umd')
 
 module.exports = function config() {
-  // console.log(waffle);
-  // if(waffle.autoEntry){
-  //   autoEntryCore();
-  // }
-  // if(waffle.build==='rollup'){
-  //   rollup(rollupUmd)
-  // }
-  rollup(rollupUmd)
+  if(waffle.autoEntry){
+    autoEntryCore();
+  }
+  if(waffle.build==='rollup'){
+    return
+  }
 }
 
 
